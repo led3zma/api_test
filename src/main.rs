bb1ced9601f14c47a79e6c07e8c58db94fe56f75
Alejandro Ledezma <alejandro.ledezma16@gmail.com>
2023-10-20 15:34:35 -0400

    Implement simple API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Info {
+    message: String,
+}
+
+async fn index() -> impl Responder {
+    web::Json(Info {
+        message: "Hello World!".into(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
