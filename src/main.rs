31db150c2a16785ed1f83f654348ffae2876284b
Alejandro Ledezma <alejandro.ledezma16@gmail.com>
2023-10-20 16:00:18 -0400

    Implement frontend

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{web, App, HttpServer, Responder};
+use std::fs;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use log::info;
 use serde::Serialize;
 
@@ -7,19 +9,29 @@ struct Info {
     message: String,
 }
 
-async fn index() -> impl Responder {
+async fn api() -> impl Responder {
     info!("Handling index request!");
     web::Json(Info {
         message: "Hello World!".into(),
     })
 }
 
+async fn index() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(fs::read_to_string("frontend/index.html").unwrap())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     info!("Starting server...");
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:3000")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/api/message", web::get().to(api))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
